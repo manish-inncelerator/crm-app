628d2dc2936abe4a640f9a2c5919394fbec56cd5
manish-inncelerator <manish.inncelerator@gmail.com>
2026-04-30 23:31:30 +0530

    Fix path issues in migration script

--- database/migration.php
+++ database/migration.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../vendor/autoload.php';
-require_once '../database.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../database.php';
 
 /**
  * CRM to Financial Monitoring System Migration Script
